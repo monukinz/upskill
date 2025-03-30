d6f5a2ab159b28af02d98140b498c42324c3aad9
Mona <127320170+monukinz@users.noreply.github.com>
2025-03-30 18:12:51 +0200

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,15 @@
+# Homework1
+num1 = float(input("Enter the first number:"))
+print(type(num1))
+num2 = float(input("Enter the second number:"))
+print(type(num2))
+sum_result = (num1 + num2)
+print(sum_result)
+
+# Homework2
+num1 = float(input("Enter the first number:"))
+print(type(num1))
+num2 = float(input("Enter the second number:"))
+print(type(num2))
+exponential_result = (num1 ** num2)
+print(exponential_result)
